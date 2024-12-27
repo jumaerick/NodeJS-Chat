b9d5a439aa56b459e153c5e54283f2f0cb2ff6fc
Juma Erick <jumaaerick@gmail.com>
2024-12-27 19:57:47 +0300

    updated-project

--- public/js/script.js
+++ public/js/script.js
@@ -48,7 +48,7 @@ const handleChat = () => {
 
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("...", "incoming");
+        const incomingChatLi = createChatLi("generating responses...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         generateResponse(incomingChatLi);
