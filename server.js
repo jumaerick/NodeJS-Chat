c273dfad9a31e2064dded76a832261c83df86eaa
Juma Erick <jumaaerick@gmail.com>
2024-12-28 15:02:07 +0300

    added-cors

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 const PORT = 3000;
+
+const allowedOrigins = ['http://127.0.0.1:8000/'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // If the origin is in the allowed list or it's a server-side request (like a POST from a backend)
+      callback(null, true);
+    } else {
+      // If the origin is not allowed
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST'], // Allow only GET and POST methods
+};
+
+
 app.use(cors());
 
 // Serve static files from the "public" directory
