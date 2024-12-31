9cfe8932b5a47241c6245e954d2e26ca03cb2e59
Juma Erick <jumaaerick@gmail.com>
2024-12-31 12:10:42 +0300

    cors-options

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-const allowedOrigins = ['http://127.0.0.1:8000/'];
+const allowedOrigins = ['http://127.0.0.1:8000'];
 
 let corsOptions = {
   origin :allowedOrigins,
